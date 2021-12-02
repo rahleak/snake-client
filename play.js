b158647536fc09320030bd12be9662fbe089ed18
vagrant <rah.leak@gmail.com>
2021-12-02 23:48:54 +0000

    it moves!

--- play.js
+++ play.js
@@ -1,24 +1,8 @@
-const connect = require("./client")
+const connect = require("./client");
+const setupInput = require("./input");
+
 
 console.log("Connecting ...");
 connect();
 
-const handleUserInput = function (key) {
-  console.log(key);
-  if (key === "x") {
-    process.exit();
-  }
-};
-
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-setupInput();
+setupInput(connect());
