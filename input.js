2088648cee47ebaab890a92fd8c8687bd1f56f32
vagrant <rah.leak@gmail.com>
2021-12-02 23:22:03 +0000

    input file

---
+++ input.js
@@ -0,0 +1,17 @@
+const handleUserInput = function (key) {
+  console.log(key);
+  if (key === "x") {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+module.exports = setupInput;
