ec8468afe8c308284787ddc4e55e9f7013829a48
vagrant <rah.leak@gmail.com>
2021-12-02 23:49:50 +0000

    making moves

--- input.js
+++ input.js
@@ -1,11 +1,22 @@
+let connection;
+
 const handleUserInput = function (key) {
   console.log(key);
-  if (key === "x") {
+  if (key === "w"){
+    connection.write("Move: up")
+  } else if (key === "a"){
+    connection.write("Move: left")
+  } else if (key === "s"){
+    connection.write("Move: down")
+  } else if (key === "d"){
+    connection.write("Move: right")
+  } else if (key === "x") {
     process.exit();
   }
 };
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
