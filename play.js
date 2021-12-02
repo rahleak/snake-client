6cab1683c95eb7abb3806d9dac3421e1b72e1c34
vagrant <rah.leak@gmail.com>
2021-12-02 23:06:38 +0000

    handleUserInput

--- play.js
+++ play.js
@@ -2,3 +2,23 @@ const connect = require("./client")
 
 console.log("Connecting ...");
 connect();
+
+const handleUserInput = function (key) {
+  console.log(key);
+  if (key === "x") {
+    process.exit();
+  }
+};
+
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput();
